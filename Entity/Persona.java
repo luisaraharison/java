cd141a489e6738e4b12f16dcf38f71b543ba9d9e
Your Name <you@example.com>
2024-03-13 12:28:42 +0100

    first commit

---
+++ Entity/Persona.java
@@ -0,0 +1,7 @@
+package main.java.Entity;
+
+public class Persona {
+    String nome;
+    String cognome;
+    int eta;
+}
---
+++ com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    //this is a test
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
